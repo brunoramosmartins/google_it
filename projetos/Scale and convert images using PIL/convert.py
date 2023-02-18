dccb185f78ea460ab50d1de474fdfcd1d85129fb
brunoramosmartins <brunoramosmartins1989@gmail.com>
2023-02-17 22:28:58 -0300

    Início da solução do primeiro projeto.

---
+++ projetos/Scale and convert images using PIL/convert.py
@@ -0,0 +1,8 @@
+import os
+from PIL import Image
+
+for image in os.listdir('images'):
+    if image != '.DS_Store':
+        string = 'images/' + image
+        im = Image.open(string)
+        im.rotate(-90).resize((128, 128)).save('new_images/' + image + ".jpeg")
