e8d0260c084edef4e429af7d6230bab12f815236
brunoramosmartins <brunoramosmartins1989@gmail.com>
2023-03-03 12:08:21 -0300

    Solucao Q1 do projeto final.

--- projetos/Scale and convert images using PIL/convert.py
+++ projetos/Scale and convert images using PIL/convert.py
@@ -5,4 +5,4 @@ for image in os.listdir('images'):
     if image != '.DS_Store':
         string = 'images/' + image
         im = Image.open(string)
-        im.rotate(-90).resize((128, 128)).save('new_images/' + image + ".jpeg")
+        im.rotate(-90).resize((128, 128)).convert('RGB').save('new_images/' + image + ".jpeg", 'JPEG')
---
+++ projetos/q1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import os
+from PIL import Image
+
+for image in os.listdir('supplier-data/images'):
+    try:
+        filename = 'supplier-data/images/' + image
+        im = Image.open(filename)
+        im.resize((600, 400)).convert('RGB').save('supplier-data/images/' + image[:3] + ".jp$
+    except:
+        pass
