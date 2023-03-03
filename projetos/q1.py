4895119370a388d207803620e230479b6378d5b9
brunoramosmartins <brunoramosmartins1989@gmail.com>
2023-03-03 12:38:52 -0300

    Correcao do codigo da Q1

--- projetos/q1.py
+++ projetos/q1.py
@@ -6,6 +6,7 @@ for image in os.listdir('supplier-data/images'):
     try:
         filename = 'supplier-data/images/' + image
         im = Image.open(filename)
-        im.resize((600, 400)).convert('RGB').save('supplier-data/images/' + image[:3] + ".jp$
+        im.resize((600, 400)).convert('RGB').save('supplier-data/images/' + image[:3] + ".jpeg", 'JPEG')
+
     except:
         pass
